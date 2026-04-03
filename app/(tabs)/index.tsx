03fd26f45639a4f91cd8dd428ce70bd12992d686
YahyaPro <kingwar66@gmail.com>
2026-04-03 13:46:49 +0200

    feat: implement custom tab navigation layout and add project assets

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,12 @@
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { styled } from "nativewind";
+import { Text } from "react-native";
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+const SafeAreaView = styled(RNSafeAreaView);
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-background">
+    <SafeAreaView className="flex-1 p-5 bg-background">
       <Text className="text-xl font-bold text-success">
         Welcome to Nativewind!
       </Text>
@@ -17,9 +20,9 @@ export default function App() {
       <Link href="/(auth)/sign-up" className="text-xl font-bold text-primary">
         Go to sign up
       </Link>
-      <Link href="/(tabs)/subscriptions/1" className="text-xl font-bold text-primary">
+      <Link href="/subscriptions/1" className="text-xl font-bold text-primary">
         Go to subscription 1
       </Link>
-    </View>
+    </SafeAreaView>
   );
 }
